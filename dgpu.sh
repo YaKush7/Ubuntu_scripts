e193446f68ef3dc430642de969623f5487bfedc0
Yash Kush <47322668+Yash227@users.noreply.github.com>
2020-12-31 01:18:44 +0530

    Add files via upload

---
+++ dgpu.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# to execete program with nvidia gpu
+# when prime profile is set to --> NVIDIA ON DEMAND
+
+# set up as described in
+# https://www.reddit.com/r/linux_gaming/comments/aoh5be/guide_hybrid_graphics_on_linux_nvidia_optimus/
+# for nvidia gpu
+
+helpFunction()
+{
+   echo ""
+   echo "Usage: dgpu [Name]"
+   echo ""
+   echo "EXAMPLE:"
+   echo -e "\tdgpu glxheads :0"
+   echo ""
+   exit 1
+}
+
+# runs program with nvidia gpu
+execute()
+{
+    __NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia $1
+}
+
+# main
+RUN="$*"
+if [ -z "$RUN" ]; then
+    helpFunction
+else
+    execute $RUN
+fi
